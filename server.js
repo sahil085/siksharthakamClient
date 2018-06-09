a89255efaecd15c131dcdb2a5028feda71778f6f
sahil085 <vermasahil269@gmail.com>
2018-06-09 13:04:13 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(process.env.PWD+ '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(process.env.PWD+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/iconnect/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
