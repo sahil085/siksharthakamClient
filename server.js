6893af753a7f3a630b71dcdea4917d66544f917a
sahil085 <vermasahil269@gmail.com>
2018-06-09 23:24:31 +0530

    angular 6

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use( express.static(path.join(__dirname, '/dist/iconnect')));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/iconnect/index.html'));
+  res.sendFile(path.join('./dist/index.html'));
 });
 app.listen(process.env.PORT || 8080);
