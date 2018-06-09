925ad825921bc942b9b43a9b1a04c3c4926b461e
sahil085 <vermasahil269@gmail.com>
2018-06-09 23:16:55 +0530

    angular 6

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-// app.use(express.static(__dirname+'/src'));
-
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, '/dist')));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('./dist/iconnect/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
