657209d77250e8c02fde660ef0ad9125d1111b8b
sahil085 <vermasahil269@gmail.com>
2018-06-09 23:58:40 +0530

    angular deployment

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-app.use( express.static(path.join(__dirname, '/dist/iconnect')));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/index.html'));
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/iconnect/index.html'));
 });
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
