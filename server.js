c51307bcf14fe67e5061f6a675b02a323e6aae7f
sahil085 <vermasahil269@gmail.com>
2018-06-13 14:15:10 +0530

    appending siksharthakamclient to dist in server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/iconnect'));
+app.use(express.static(__dirname + '/dist/siksharthakamclient'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/iconnect/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/siksharthakamclient/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
